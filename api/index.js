6ed438b5574a1f174ff2ce299c46d466625a4304
Solomon <123379369+Solafides@users.noreply.github.com>
2025-02-20 10:16:41 +0300

    connestion established

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+// import mongodb from 'mongodb';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 // import { get } from './get';
 // import { post } from './post';
 
+mongoose.connect(process.env.mongourl)
+.then(
+    ()=>
+    {
+        console.log('Connected to database');
+    }
+)
+.catch(
+    (err)=>{
+        console.log(err);
+    }   );
 const app=express();
 app.use(express.json());
 // app.get('/',get);
