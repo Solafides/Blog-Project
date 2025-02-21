a82f90ce05dd5d8a0886dfe5115e16797a68550b
Solomon <123379369+Solafides@users.noreply.github.com>
2025-02-21 10:18:38 +0300

    create signup controller

--- api/controller/signup_controller.js
+++ api/controller/signup_controller.js
@@ -1,15 +1,16 @@
 import User from '../models/users.js';
 import bcrypt from 'bcryptjs';
-export const signup = async (req, res) => {
+import { errorHandler } from '../utils/errors.js';
+export const signup = async (req, res,next) => {
     const { username, email, password } = req.body;
     if (!username || !email || !password|| username==='' || email==='' || password==='') {
-        return res.status(400).json({ message: 'All fields are required' });
+       next(errorHandler(400, 'All fields are required'));
     }
     const hashpassword= bcrypt.hashSync(password, 10);
     const newuser = new User({ username, email, password: hashpassword, });
     try{await newuser.save();
     res.json({ message: 'Signup successful' });}
     catch(err){
-        return res.status(500).json({ message: err.message });
+        next(err);
     }
 };
--- api/index.js
+++ api/index.js
@@ -26,3 +26,10 @@ app.use(express.json());
 app.listen(3000,()=>console.log('Server is running on port 3000'));
 app.use('/user',userRoutes);
 app.use('/auth',signupRoutes);
+app.use((err, req, res, next)=>{
+    const status=err.status||500;
+    const message=err.message || 'internal server error';
+    res.status(status).json({success:false,
+        status,
+        message});
+});
